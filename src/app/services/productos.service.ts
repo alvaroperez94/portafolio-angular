82f327847e143aada742ae293a764d2e2bcea39d
Álvaro Pérez <alvaroperez4c@gmail.com>
2023-10-04 23:33:28 +0200

    Buscador

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,22 +9,56 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-1da78-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
+    return new Promise<void>((resolve, reject) => {
+      this.http.get('https://angular-html-1da78-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
       .subscribe((resp: any) => {
         // No me funciona poniendo la interfaz de Producto en vez del any
         this.productos = resp;
         this.cargando = false;
+        resolve();
       });
+    });
   }
 
   getProducto(id: string) {
     return this.http.get(`https://angular-html-1da78-default-rtdb.europe-west1.firebasedatabase.app/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string) {
+    if (this.productos.length === 0) {
+      // Cargar los productos
+      this.cargarProductos().then(() => {
+        // Ejecutar después de tener los productos
+        // Aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      // Aplicar filtro
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos (termino: string) {
+    // console.log(this.productos);
+
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+      }
+    });
   }
 
 }
